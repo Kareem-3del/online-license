0dc361926bb36affb7c06a4babe0e2231dd1dc6c
Kareem-Adel <75784840+Kareem-3del@users.noreply.github.com>
2024-07-01 04:52:08 +0300

    Update daralaleia.js

--- daralaleia.js
+++ daralaleia.js
@@ -1,1 +1,9 @@
-alert("Active");
+function replaceElement() {
+    const rootElement = document.getElementById('root');
+    const newHeading = document.createElement('h1');
+    newHeading.textContent = "لم يتم دفع المستحقات";
+
+    // Replace the existing content with the new heading
+    rootElement.innerHTML = ''; // Clear existing content
+    rootElement.appendChild(newHeading);
+}
